dd9c2b3a6d89c0741f025236fcee3dada8d1e189
Rishav Singh <140177659+Rishavsinghf@users.noreply.github.com>
2023-11-22 11:35:41 +0000

    small json

---
+++ basics/objectsapi.js
@@ -0,0 +1,20 @@
+const course = {
+    coursename: "javascript full",
+    price: 15000,
+    courseInstuctor: "Rishav"
+}
+
+// console.log(course.courseInstuctor);
+
+const { courseInstuctor } = course
+
+console.log(courseInstuctor);
+
+// APIS 
+// JSON 
+
+// {
+//     "name": "Rishav",
+//     "coursename": "Javascript",
+//     "price": "free"
+// }
