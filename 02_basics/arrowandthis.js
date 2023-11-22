1ff0636933d9b61ff92d9ac6b91d4942f9bd4ee7
Rishav Singh <140177659+Rishavsinghf@users.noreply.github.com>
2023-11-22 20:42:08 +0000

    Arrow function done

---
+++ 02_basics/arrowandthis.js
@@ -0,0 +1,43 @@
+const user = {
+    username : "Rishav",
+    price : 599,
+
+    welcomeMessage : function() {
+        // console.log(`${this.username}, welcome to website`);
+    }
+}
+
+// user.welcomeMessage()
+
+// user.username = " Sam"
+// user.welcomeMessage()
+
+// function chai() {
+//     console.log(this);
+// }
+// chai()
+
+const chai = () => {
+    let username = " Rishav"
+    console.log(username);
+}
+
+// chai()
+
+// exciplict 
+//  const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+// Implicit
+// const addTwo = (num1, num2) =>   num1 + num2
+
+// const addTwo = (num1, num2) =>   (num1 + num2)
+
+// console.log(addTwo(3,4));
+
+
+
+
+
